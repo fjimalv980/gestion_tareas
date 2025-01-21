bb72783d90da1ae79be68eaf4ff7919cf4374527
Francisco <fjimalv980@g.educaand.es>
2025-01-21 08:51:59 +0100

    Editado archivo __manifest__.py”

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+{
+'name': 'Gestión de Tareas',
+'version': '1.0',
+'summary': 'Módulo para gestionar tareas individuales de los empleados',
+'category': 'Productivity',
+'author': 'Francisco Jiménez',
+'website': 'https://tuweb.com',
+'license': 'LGPL-3',
+'depends': ['base', 'mail'],
+'icon': '/gestion_tareas/static/description/icon52.png',
+'data': [
+'views/gestion_tarea_views.xml',
+'security/ir.model.access.csv',
+],
+'application': True,
+'installable': True,
+'auto_install': False,
+'description': """
+Módulo de Odoo para la gestión de tareas asignadas a empleados,
+incluyendo vistas Kanban y formulario detallado.
+"""
+}
